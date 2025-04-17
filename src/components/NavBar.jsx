75c56047469ec1cdcc2bc144de2cd44a1aacd202
Sumit Mukharjee <sumit1mukharjee@gmail.com>
2025-04-17 19:53:30 +0530

    navbar done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,27 @@ import './NavBar.css';
 
 function Navbar() {
     return (
-        <h1>this is NavBar</h1>
+        <nav className="navbar">
+        <div className="container">
+          <a href="#" className="navbar-brand">Sumit Mukharjee</a>
+          
+          <div className="navbar-content">
+            <div className="details">
+              <a href="#home">Home</a>
+              <a href="#about">About</a>
+              <a href="#experience">Experience</a>
+              <a href="#projects">Projects</a>
+            </div>
+            <div className="icons">
+              <a href="" className="nav-links">Mail</a>
+              <a href="" className="nav-links">Git</a>
+              <a href="" className="nav-links">Linked</a>
+              <a href="" className="nav-links">Hashnode</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      
     )
 }
 export default Navbar;
